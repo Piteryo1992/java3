7550f616a2e1f6ebe2ce2d466d2067e496db5f70
Piotr <piotr_z6@wp.pl>
2018-12-18 16:32:10 +0100

    Pierwszy commit javy z intelliJ

---
+++ src/main/java/pl/bae/kolekcje/Person.java
@@ -0,0 +1,19 @@
+package pl.bae.kolekcje;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private String lastName;
+    private int id;
+
+
+
+
+}
